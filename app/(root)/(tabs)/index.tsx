ed6694121c85df16f6cd39d6996f3af1f2691b34
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-01-08 11:43:49 +0200

    explore screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -47,7 +47,7 @@ export default function Index() {
 			<FlatList 
 			data={properties}
 			renderItem={({ item }) => <Card item={item} onPress={() => handleCardPress(item.$id)}/>}
-			keyExtractor={(item) => item.toString()}
+			keyExtractor={(item) => item.$id}
 			numColumns={2}
 			contentContainerClassName="pb-32"
 			columnWrapperClassName="flex gap-5 px-5"
@@ -92,7 +92,7 @@ export default function Index() {
 				<FlatList 
 				data={latestProperties} 
 				renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)}/>}
-				keyExtractor={(item) => item.toString()}
+				keyExtractor={(item) => item.$id}
 				horizontal
 				bounces={false}
 				showsHorizontalScrollIndicator={false}
