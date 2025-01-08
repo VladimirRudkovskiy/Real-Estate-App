517e4a9ca1afdba41c118ae7433e3651bdb3005a
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-01-08 10:45:39 +0200

    seeding database and fetching queries

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -15,7 +15,7 @@ export const FeaturedCard = ({ onPress }: Props) => {
 
 			<View className='flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5'>
 				<Image source={icons.star} className='size-3.5'/>
-				<Text className='text-xs font-rubik-bold text-primary-300 ml-1'>4.4</Text>
+					<Text className='text-xs font-rubik-bold text-primary-300 ml-1'>4.4</Text>
 			</View>
 
 			<View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
@@ -48,7 +48,7 @@ export const Card = ({ onPress }: Props) => {
 			
 			<View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1  rounded-full z-50'>
 				<Image source={icons.star} className='size-2.5'/>
-				<Text className='text-xs font-rubik-bold text-primary-300 ml-0.5'>4.4</Text>
+					<Text className='text-xs font-rubik-bold text-primary-300 ml-0.5'>4.4</Text>
 			</View>
 
 			<Image source={images.newYork} className='w-full h-40 rounded-lg'/>
